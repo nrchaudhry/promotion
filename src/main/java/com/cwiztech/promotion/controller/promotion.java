1c5c1fb997d0d68428166305a3662c9d23c21b1c
faiza-batool-dev <faizabatool12862@gmail.com>
2025-12-03 21:10:21 +0500

    new commit

--- src/main/java/com/cwiztech/promotion/controller/promotion.java
+++ src/main/java/com/cwiztech/promotion/controller/promotion.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.cwiztech.promotion.model.PromotionModel;
 import com.cwiztech.promotion.repository.promotionRepository;
@@ -27,6 +30,11 @@ public class promotion {
 		List<PromotionModel> promotion = repo.findAll();
 
 		return promotion;
+	}
+	
+	@PostMapping("/add")
+	public PromotionModel addEmployee(@RequestBody PromotionModel promotion) {
+	    return repo.save(promotion); // saves employee to DB and returns saved object
 	}
 
 }
