e8de7bea0c18704b91e0fbb548a713422951407e
faiza-batool-dev <faizabatool12862@gmail.com>
2025-12-09 21:10:55 +0500

    New Changes

--- src/main/java/com/cwiztech/PromotionApplication.java
+++ src/main/java/com/cwiztech/PromotionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.cwiztech.promotion")
+// @ComponentScan("com.cwiztech.promotion")
 public class PromotionApplication {
 
 	public static void main(String[] args) {
